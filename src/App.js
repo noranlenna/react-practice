8588b9659af4e095634afcfaf920d685547fddd3
noranlenna <noranlenna@gmail.com>
2022-07-12 13:46:52 +0900

    remove velopert part

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
-
-import MultipleInput from "./component/velopert-web/1/9.multipleinputs/MultipleInput";
-
-
 function App() {
   return (
     <div className="App">
       <div className="react-practice">
-        <MultipleInput></MultipleInput>
+
       </div>
     </div>
   );
